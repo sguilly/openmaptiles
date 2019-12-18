fe89f874127746c406b513623d941a2d21353801
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2019-12-18 14:29:49 +0100

    Support differential update of buildings clusters

--- layers/building/building.sql
+++ layers/building/building.sql
@@ -4,7 +4,7 @@
 CREATE INDEX IF NOT EXISTS osm_building_relation_building_idx ON osm_building_relation(building) WHERE building = '' AND ST_GeometryType(geometry) = 'ST_Polygon';
 CREATE INDEX IF NOT EXISTS osm_building_relation_member_idx ON osm_building_relation(member) WHERE role = 'outline';
 
-CREATE OR REPLACE VIEW osm_all_buildings AS (
+CREATE OR REPLACE VIEW osm_buildings_relation AS
          -- etldoc: osm_building_relation -> layer_building:z14_
          -- Buildings built from relations
          SELECT member AS osm_id, geometry,
@@ -17,8 +17,9 @@ CREATE OR REPLACE VIEW osm_all_buildings AS (
                   FALSE as hide_3d
          FROM
          osm_building_relation WHERE building = '' AND ST_GeometryType(geometry) = 'ST_Polygon'
-         UNION ALL
+;
 
+CREATE OR REPLACE VIEW osm_buildings_standalone AS
          -- etldoc: osm_building_polygon -> layer_building:z14_
          -- Standalone buildings
          SELECT obp.osm_id, obp.geometry,
@@ -36,10 +37,15 @@ CREATE OR REPLACE VIEW osm_all_buildings AS (
              obr.member = obp.osm_id AND
              obr.role = 'outline'
          WHERE ST_GeometryType(obp.geometry) IN ('ST_Polygon', 'ST_MultiPolygon')
-);
+;
 
-DROP MATERIALIZED VIEW IF EXISTS osm_all_buildings_mat CASCADE;
-CREATE MATERIALIZED VIEW osm_all_buildings_mat AS (
+CREATE OR REPLACE VIEW osm_all_buildings AS
+SELECT * FROM osm_buildings_relation UNION ALL
+SELECT * FROM osm_buildings_standalone
+;
+
+DROP TABLE IF EXISTS osm_all_buildings_mat CASCADE;
+CREATE TABLE osm_all_buildings_mat AS (
     SELECT
         --max(osm_id) AS osm_id,
         ST_Collect(geometry) AS geometry,
@@ -115,6 +121,32 @@ LANGUAGE SQL IMMUTABLE;
 -- Handle updates
 CREATE SCHEMA IF NOT EXISTS building_polygon;
 
+CREATE TABLE IF NOT EXISTS building_polygon.buildings(id serial primary key, osm_id bigint, old_geometry geometry(Geometry,3857), new_geometry geometry(Geometry,3857));
+CREATE OR REPLACE FUNCTION building_polygon.store() RETURNS trigger AS $$
+BEGIN
+    IF (TG_OP = 'DELETE') THEN
+        IF ST_GeometryType(OLD.geometry) = 'ST_Polygon' THEN
+            INSERT INTO building_polygon.buildings(osm_id, old_geometry, new_geometry)
+                VALUES (OLD.osm_id, OLD.geometry, NULL::geometry);
+        END IF;
+    ELSIF (TG_OP = 'UPDATE') THEN
+        IF ST_GeometryType(OLD.geometry) = 'ST_Polygon' OR ST_GeometryType(NEW.geometry) = 'ST_Polygon' THEN
+            INSERT INTO building_polygon.buildings(osm_id, old_geometry, new_geometry)
+                VALUES (
+                    NEW.osm_id,
+                    CASE WHEN ST_GeometryType(OLD.geometry) = 'ST_Polygon' THEN OLD.geometry END,
+                    CASE WHEN ST_GeometryType(NEW.geometry) = 'ST_Polygon' THEN NEW.geometry END);
+        END IF;
+    ELSIF (TG_OP = 'INSERT') THEN
+        IF ST_GeometryType(NEW.geometry) = 'ST_Polygon' THEN
+            INSERT INTO building_polygon.buildings(osm_id, old_geometry, new_geometry)
+                VALUES (NEW.osm_id, NULL::geometry, NEW.geometry);
+        END IF;
+    END IF;
+    RETURN NULL;
+END;
+$$ language plpgsql;
+
 CREATE TABLE IF NOT EXISTS building_polygon.updates(id serial primary key, t text, unique (t));
 CREATE OR REPLACE FUNCTION building_polygon.flag() RETURNS trigger AS $$
 BEGIN
@@ -126,16 +158,133 @@ $$ language plpgsql;
 CREATE OR REPLACE FUNCTION building_polygon.refresh() RETURNS trigger AS
   $BODY$
   BEGIN
-    RAISE LOG 'Refresh building_polygon';
-    REFRESH MATERIALIZED VIEW osm_all_buildings_mat;
+    RAISE LOG 'Update osm_all_buildings_mat';
+
+    -- Compact the change history to keep only the first and last version
+    CREATE TEMP TABLE old_new_buildings AS
+    SELECT DISTINCT ON (osm_id)
+        osm_id,
+        old_geometry,
+        new_geometry
+    FROM (
+        SELECT
+            osm_id,
+            first_value(old_geometry) OVER (PARTITION BY osm_id ORDER BY id) AS old_geometry,
+            last_value(new_geometry) OVER (PARTITION BY osm_id ORDER BY id) AS new_geometry
+        FROM
+            building_polygon.buildings
+    ) AS t;
+
+    -- Flatten old and new version of building
+    CREATE TEMP VIEW touched_buildings AS
+    SELECT DISTINCT(geometry) geometry
+    FROM (SELECT unnest(ARRAY[old_geometry, new_geometry]) AS geometry FROM old_new_buildings) AS t
+    WHERE geometry IS NOT NULL
+    ;
+
+    -- Seach for clusters of changed buildings
+    CREATE TEMP TABLE impacted_clusters AS
+    SELECT
+        osm_all_buildings_mat.geometry,
+        height, min_height, levels, min_level, material, colour, hide_3d
+    FROM
+        osm_all_buildings_mat
+        JOIN touched_buildings AS buildings ON
+            buildings.geometry && osm_all_buildings_mat.geometry
+    ;
+
+    -- Remove old version of impacted clusters
+    DELETE FROM osm_all_buildings_mat
+    USING impacted_clusters WHERE
+        osm_all_buildings_mat.geometry && impacted_clusters.geometry AND
+        osm_all_buildings_mat.geometry = impacted_clusters.geometry
+    ;
+
+    CREATE TEMP TABLE old_buildings AS SELECT old_geometry AS geometry FROM old_new_buildings WHERE old_geometry IS NOT NULL;
+    CREATE INDEX old_buildings_geom ON old_buildings USING gist(geometry);
+    CREATE TEMP VIEW new_buildings AS SELECT new_geometry AS geometry FROM old_new_buildings WHERE new_geometry IS NOT NULL;
+
+    -- Get new version of buildings with full attributes
+    CREATE TEMP VIEW new_buildings_full AS
+    SELECT osm_buildings.*
+    FROM new_buildings
+        JOIN osm_buildings_relation AS osm_buildings ON
+            osm_buildings.geometry && new_buildings.geometry AND
+            osm_buildings.geometry = new_buildings.geometry
+    UNION ALL
+    SELECT osm_buildings.*
+    FROM new_buildings
+        JOIN osm_buildings_standalone AS osm_buildings ON
+            osm_buildings.geometry && new_buildings.geometry AND
+            osm_buildings.geometry = new_buildings.geometry
+    ;
+
+    -- Unpack impacted clusters
+    CREATE TEMP VIEW unclustered_buildings AS
+    SELECT
+        (ST_Dump(geometry)).geom AS geometry,
+        height, min_height, levels, min_level, material, colour, hide_3d
+    FROM
+        impacted_clusters
+    ;
+
+    -- Discart old buildings from clusters
+    CREATE TEMP VIEW untouched_buildings AS
+    SELECT
+        unclustered_buildings.geometry,
+        height, min_height, levels, min_level, material, colour, hide_3d
+    FROM
+        unclustered_buildings
+        LEFT JOIN old_buildings ON
+            old_buildings.geometry && unclustered_buildings.geometry AND
+            old_buildings.geometry = unclustered_buildings.geometry
+    WHERE
+        old_buildings.geometry IS NULL
+    ;
+
+    -- Reassemble previous untouched buildings and new buildings
+    CREATE TEMP VIEW current_buildings AS
+    SELECT * FROM untouched_buildings
+    UNION
+    SELECT geometry, height, min_height, levels, min_level, material, colour, hide_3d FROM new_buildings_full
+    ;
+
+    -- Build and save new clusters
+    INSERT INTO osm_all_buildings_mat
+    SELECT
+        ST_Collect(geometry) AS geometry,
+        height, min_height, levels, min_level, material, colour, hide_3d
+    FROM
+        current_buildings AS t
+    GROUP BY
+        -- Cluster by windows to lower time and memory required
+        (ST_XMin(geometry) / 100)::int,
+        (ST_YMin(geometry) / 100)::int,
+        height, min_height, levels, min_level, material, colour, hide_3d
+    ;
+
+    DELETE FROM building_polygon.buildings;
     DELETE FROM building_polygon.updates;
+    DROP TABLE old_new_buildings CASCADE;
+    DROP TABLE impacted_clusters CASCADE;
+    DROP TABLE old_buildings CASCADE;
     RETURN null;
   END;
   $BODY$
 language plpgsql;
 
-CREATE TRIGGER trigger_flag
+CREATE TRIGGER trigger_osm_building_relation_store
+    AFTER INSERT OR UPDATE OR DELETE ON osm_building_relation
+    FOR EACH ROW
+    EXECUTE PROCEDURE building_polygon.store();
+
+CREATE TRIGGER trigger_osm_building_polygon_store
     AFTER INSERT OR UPDATE OR DELETE ON osm_building_polygon
+    FOR EACH ROW
+    EXECUTE PROCEDURE building_polygon.store();
+
+CREATE TRIGGER trigger_flag
+    AFTER INSERT ON building_polygon.buildings
     FOR EACH STATEMENT
     EXECUTE PROCEDURE building_polygon.flag();
 
