ec19267cdbef71f31f2e5a15ad73fe45c38071fa
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2020-06-03 11:47:11 +0200

    Add timers to trigger function

--- layers/housenumber/housenumber_centroid.sql
+++ layers/housenumber/housenumber_centroid.sql
@@ -55,6 +55,8 @@ $$ LANGUAGE plpgsql;
 
 CREATE OR REPLACE FUNCTION housenumber.refresh() RETURNS trigger AS
 $$
+DECLARE
+    t TIMESTAMP WITH TIME ZONE := clock_timestamp();
 BEGIN
     RAISE LOG 'Refresh housenumber';
     PERFORM convert_housenumber_point(false);
@@ -62,6 +64,8 @@ BEGIN
     DELETE FROM housenumber.osm_ids;
     -- noinspection SqlWithoutWhere
     DELETE FROM housenumber.updates;
+
+    RAISE LOG 'Refresh housenumber done in %', age(clock_timestamp(), t);
     RETURN NULL;
 END;
 $$ LANGUAGE plpgsql;
