1dd5a8f230865650e44bfec5bc5a0ea4e631c4df
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2020-06-26 17:06:25 +0200

    Make update_continent_point use incremental update #814

--- layers/place/update_continent_point.sql
+++ layers/place/update_continent_point.sql
@@ -1,57 +1,34 @@
-DROP TRIGGER IF EXISTS trigger_flag ON osm_continent_point;
-DROP TRIGGER IF EXISTS trigger_refresh ON place_continent_point.updates;
+DROP TRIGGER IF EXISTS trigger_update_point ON osm_continent_point;
 
 -- etldoc:  osm_continent_point ->  osm_continent_point
-CREATE OR REPLACE FUNCTION update_osm_continent_point() RETURNS void AS
+CREATE OR REPLACE FUNCTION update_osm_continent_point(new_osm_id bigint) RETURNS void AS
 $$
 BEGIN
     UPDATE osm_continent_point
     SET tags = update_tags(tags, geometry)
-    WHERE COALESCE(tags->'name:latin', tags->'name:nonlatin', tags->'name_int') IS NULL;
+    WHERE (new_osm_id IS NULL OR osm_id = new_osm_id) AND
+          COALESCE(tags->'name:latin', tags->'name:nonlatin', tags->'name_int') IS NULL;
 
 END;
 $$ LANGUAGE plpgsql;
 
-SELECT update_osm_continent_point();
+SELECT update_osm_continent_point(NULL);
 
 -- Handle updates
 
 CREATE SCHEMA IF NOT EXISTS place_continent_point;
 
-CREATE TABLE IF NOT EXISTS place_continent_point.updates
-(
-    id serial PRIMARY KEY,
-    t  text,
-    UNIQUE (t)
-);
-CREATE OR REPLACE FUNCTION place_continent_point.flag() RETURNS trigger AS
+CREATE OR REPLACE FUNCTION place_continent_point.update() RETURNS trigger AS
 $$
 BEGIN
-    INSERT INTO place_continent_point.updates(t) VALUES ('y') ON CONFLICT(t) DO NOTHING;
+    PERFORM update_osm_continent_point(NEW.osm_id);
     RETURN NULL;
 END;
 $$ LANGUAGE plpgsql;
 
-CREATE OR REPLACE FUNCTION place_continent_point.refresh() RETURNS trigger AS
-$$
-BEGIN
-    RAISE LOG 'Refresh place_continent_point';
-    PERFORM update_osm_continent_point();
-    -- noinspection SqlWithoutWhere
-    DELETE FROM place_continent_point.updates;
-    RETURN NULL;
-END;
-$$ LANGUAGE plpgsql;
-
-CREATE TRIGGER trigger_flag
-    AFTER INSERT OR UPDATE OR DELETE
+CREATE CONSTRAINT TRIGGER trigger_update_point
+    AFTER INSERT OR UPDATE
     ON osm_continent_point
-    FOR EACH STATEMENT
-EXECUTE PROCEDURE place_continent_point.flag();
-
-CREATE CONSTRAINT TRIGGER trigger_refresh
-    AFTER INSERT
-    ON place_continent_point.updates
     INITIALLY DEFERRED
     FOR EACH ROW
-EXECUTE PROCEDURE place_continent_point.refresh();
+EXECUTE PROCEDURE place_continent_point.update();
