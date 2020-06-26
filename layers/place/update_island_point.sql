08a7c8db64588d49192b2f6295496e6476a6ffe5
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2020-06-26 17:06:40 +0200

    Make update_island_point use incremental update #814

--- layers/place/update_island_point.sql
+++ layers/place/update_island_point.sql
@@ -1,57 +1,47 @@
-DROP TRIGGER IF EXISTS trigger_flag ON osm_island_point;
-DROP TRIGGER IF EXISTS trigger_refresh ON place_island_point.updates;
+DROP TRIGGER IF EXISTS trigger_update_point ON osm_island_point;
 
 -- etldoc:  osm_island_point ->  osm_island_point
-CREATE OR REPLACE FUNCTION update_osm_island_point() RETURNS void AS
+CREATE OR REPLACE FUNCTION update_osm_island_point(rec osm_island_point) RETURNS osm_island_point AS
 $$
 BEGIN
-    UPDATE osm_island_point
-    SET tags = update_tags(tags, geometry)
-    WHERE COALESCE(tags->'name:latin', tags->'name:nonlatin', tags->'name_int') IS NULL;
+    IF COALESCE(rec.tags->'name:latin', rec.tags->'name:nonlatin', rec.tags->'name_int') IS NULL THEN
+        rec.tags = update_tags(rec.tags, rec.geometry);
+    END IF;
 
+    RETURN rec;
 END;
 $$ LANGUAGE plpgsql;
 
-SELECT update_osm_island_point();
+DO
+$$
+DECLARE
+    orig osm_island_point;
+    up osm_island_point;
+BEGIN
+    FOR orig IN SELECT * FROM osm_island_point
+    LOOP
+        up := update_osm_island_point(orig);
+        IF orig.* IS DISTINCT FROM up.* THEN
+            DELETE FROM osm_island_point WHERE osm_island_point.id = up.id;
+            INSERT INTO osm_island_point VALUES (up.*);
+        END IF;
+    END LOOP;
+END;
+$$ LANGUAGE plpgsql;
 
 -- Handle updates
 
 CREATE SCHEMA IF NOT EXISTS place_island_point;
 
-CREATE TABLE IF NOT EXISTS place_island_point.updates
-(
-    id serial PRIMARY KEY,
-    t text,
-    UNIQUE (t)
-);
-CREATE OR REPLACE FUNCTION place_island_point.flag() RETURNS trigger AS
+CREATE OR REPLACE FUNCTION place_island_point.update() RETURNS trigger AS
 $$
 BEGIN
-    INSERT INTO place_island_point.updates(t) VALUES ('y') ON CONFLICT(t) DO NOTHING;
-    RETURN NULL;
+    RETURN update_osm_island_point(NEW);
 END;
 $$ LANGUAGE plpgsql;
 
-CREATE OR REPLACE FUNCTION place_island_point.refresh() RETURNS trigger AS
-$$
-BEGIN
-    RAISE LOG 'Refresh place_island_point';
-    PERFORM update_osm_island_point();
-    -- noinspection SqlWithoutWhere
-    DELETE FROM place_island_point.updates;
-    RETURN NULL;
-END;
-$$ LANGUAGE plpgsql;
-
-CREATE TRIGGER trigger_flag
-    AFTER INSERT OR UPDATE OR DELETE
+CREATE TRIGGER trigger_update_point
+    BEFORE INSERT OR UPDATE
     ON osm_island_point
-    FOR EACH STATEMENT
-EXECUTE PROCEDURE place_island_point.flag();
-
-CREATE CONSTRAINT TRIGGER trigger_refresh
-    AFTER INSERT
-    ON place_island_point.updates
-    INITIALLY DEFERRED
     FOR EACH ROW
-EXECUTE PROCEDURE place_island_point.refresh();
+EXECUTE PROCEDURE place_island_point.update();
